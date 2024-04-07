61a5dd4f616231fb47e52567d743130407c1d604
ShleshaSrivastava <122985181+ShleshaSrivastava@users.noreply.github.com>
2024-04-07 14:48:57 +0530

    chore: Setup eslint and prettier for project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import Homepage from './components/Homepage';
-import Loginpage from './components/Loginpage';
-import { Routes, Route } from "react-router-dom"
+import './App.css'
+import Homepage from './components/Homepage'
+import Loginpage from './components/Loginpage'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={ <Homepage/> } />
-        <Route path="login" element={ <Loginpage/> } />
+        <Route path="/" element={<Homepage />} />
+        <Route path="login" element={<Loginpage />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
